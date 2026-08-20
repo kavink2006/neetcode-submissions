d71e08b8fe882065d0da4859f2a9054821722e8a
Kavin Kangeyan <102920196+kavink2006@users.noreply.github.com>
2026-08-20 11:31:12 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        
+        int counts[26] = {0}; 
+        
+        for (int i = 0; i < s.size(); i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--; 
+        }
+        
+        for (int count : counts) {
+            if (count != 0) return false; 
+        }
+        
+        return true;
+    }
+};
